0b333dcc059eb8d347f73e3351ca936be9347e5a
Cara Dodge <shxpxshxftxr@gmail.com>
2022-03-20 12:08:07 -0400

    html and port setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const http = require("http").Server(app);
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+http.listen(PORT, function () {
+  console.log("listening on *:here - origin set");
+});
